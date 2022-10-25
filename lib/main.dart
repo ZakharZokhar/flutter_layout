6948ff3e08e21a7b5daaae9fe69087558f51574c
Novikov Zakhar <znovikov@aspirity.com>
2022-10-25 11:26:36 +0700

    Исправления

--- lib/main.dart
+++ lib/main.dart
@@ -46,60 +46,95 @@ Widget topBar = Container(
     height: 100,
     padding: const EdgeInsets.fromLTRB(20, 20, 20, 5),
     color: Colors.white,
-    child: Column(children: [
-      const Align(
-        alignment: Alignment.centerLeft,
-        child: Text(
-          'Sales History',
-          textAlign: TextAlign.left,
-          style: TextStyle(fontSize: 20),
-        ),
+    child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      const Text(
+        'Sales History',
+        textAlign: TextAlign.left,
+        style: TextStyle(fontSize: 20),
       ),
       const SizedBox(
         height: 20,
       ),
       Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Expanded(
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: Container(
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.indigo.shade600),
-                    borderRadius: BorderRadius.circular(5)),
-                child: Icon(
-                  Icons.filter_alt,
-                  color: Colors.indigo[600],
-                ),
-              ),
+          Container(
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.indigo.shade600),
+                borderRadius: BorderRadius.circular(5)),
+            child: Icon(
+              Icons.filter_alt,
+              color: Colors.indigo[600],
             ),
           ),
-          const Padding(
-            padding: EdgeInsets.only(right: 20),
-            child: Text('Last week'),
-          ),
-          const Icon(Icons.expand_more)
+          Row(
+            children: const [
+              Padding(
+                padding: EdgeInsets.only(right: 20),
+                child: Text('Last week'),
+              ),
+              Icon(Icons.expand_more)
+            ],
+          )
         ],
       ),
     ]));
 
-Widget purshaseList = ListView(children: [
-  buildCard('images/1.png', '\$100', 'Cool headphones', 'today'),
-  buildCard('images/2.jpg', '\$200', 'Good headphones', 'tommorow'),
-  buildCard('images/3.jpg', '\$300', 'Very good good good good good headphones',
-      'yesterday'),
-  buildCard('images/4.jpg', '\$400', 'Excellent headphones', 'never'),
-  buildCard('images/5.jpg', '\$500', 'Marvelous headphones', 'today'),
-  buildCard('images/6.jpg', '\$1000', 'Just headphones', 'today'),
-  buildCard('images/7.jpg', '\$1500', 'Headphones', 'today'),
+Widget purshaseList = ListView(children: const [
+  PurchaseCard(
+      image: 'images/1.png',
+      price: '\$100',
+      name: 'Cool headphones',
+      day: 'today'),
+  PurchaseCard(
+      image: 'images/2.jpg',
+      price: '\$200',
+      name: 'Good headphones',
+      day: 'tommorow'),
+  PurchaseCard(
+      image: 'images/3.jpg',
+      price: '\$300',
+      name: 'Very good headphones',
+      day: 'yesterday'),
+  PurchaseCard(
+      image: 'images/4.jpg',
+      price: '\$400',
+      name: 'Excellent headphones hhuhduh dwhduh',
+      day: 'never'),
+  PurchaseCard(
+      image: 'images/5.jpg',
+      price: '\$500',
+      name: 'Marvelous headphones',
+      day: 'today'),
+  PurchaseCard(
+      image: 'images/6.jpg',
+      price: '\$1000',
+      name: 'Just headphones',
+      day: 'today'),
+  PurchaseCard(
+      image: 'images/7.jpg', price: '\$1500', name: 'Headphones', day: 'today'),
 ]);
 
-Card buildCard(String image, String price, String name, String day) {
-  return Card(
-    child: Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: IntrinsicHeight(
+class PurchaseCard extends StatelessWidget {
+  const PurchaseCard(
+      {super.key,
+      required this.image,
+      required this.name,
+      required this.price,
+      required this.day});
+
+  final String image;
+  final String name;
+  final String price;
+  final String day;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
         child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             ClipRRect(
               borderRadius: BorderRadius.circular(15),
@@ -110,40 +145,38 @@ Card buildCard(String image, String price, String name, String day) {
               ),
             ),
             Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(left: 10),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Container(
-                          constraints:
-                              BoxConstraints(maxWidth: 200, maxHeight: 100),
-                          child: Text(
-                            name,
-                            style: const TextStyle(fontSize: 18),
-                          ),
-                        ),
-                        Text(
-                          day,
-                          style: const TextStyle(color: Colors.grey),
-                        )
-                      ],
-                    ),
-                    Align(
-                        alignment: Alignment.bottomRight,
-                        child: Text(price,
-                            style: const TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold)))
-                  ],
-                ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Container(
+                        constraints:
+                            const BoxConstraints(maxWidth: 200, maxHeight: 100),
+                        child: Text(name, style: const TextStyle(fontSize: 18)),
+                      )),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        day,
+                        style: const TextStyle(color: Colors.grey),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Text(price,
+                          style: const TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold))
+                    ],
+                  ),
+                ],
               ),
             )
           ],
         ),
       ),
-    ),
-  );
+    );
+  }
 }
